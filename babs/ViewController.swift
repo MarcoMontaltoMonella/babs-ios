f9f582a7d7c9a7276e93c373f17be75faf068070
Marco Montalto Monella <marco.montalto.93@gmail.com>
2015-07-16 16:27:32 +0200

    Initial Commit

---
+++ babs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babs
+//
+//  Created by Marco Montalto Monella on 16/07/2015.
+//  Copyright (c) 2015 Marco Montalto Monella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
